b75ac1a9d98c843e0750e230e7c0c08aa5976f91
Sanjay Singh <77540699+MimoHasPurpose@users.noreply.github.com>
2025-03-18 12:56:11 +0530

    update

--- prerequisites/main.js
+++ prerequisites/main.js
@@ -1,9 +1,19 @@
-const canvas=document.getElementById("myCanvas");
-const ctx=canvas.getContext("2d");
+const canvas = document.getElementById("mynn");
+const ctx = canvas.getContext("2d");
+
+// Set line width
+ctx.lineWidth = 10;
+
+// Wall
+ctx.strokeRect(75, 140, 150, 110);
+
+// Door
+ctx.fillRect(130, 190, 40, 60);
+
+// Roof
 ctx.beginPath();
-ctx.moveTo(300,300);
-ctx.lineTo(400,300);
+ctx.moveTo(50, 140);
+ctx.lineTo(150, 60);
+ctx.lineTo(250, 140);
+ctx.closePath();
 ctx.stroke();
-ctx.font = "20px Georgia";
-ctx.fillText("Hello World!", 300,300);
-ctx.fillText("continued",300,319);
